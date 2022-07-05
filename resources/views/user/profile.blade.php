4ddd09ebad89d235584493825e93cf33d4a298c1
nakedsneak <g1931152@m.sojo-u.ac.jp>
2022-07-05 23:39:25 +0900

    add BasicView and make HomeView

---
+++ resources/views/user/profile.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<title>Every Buddy</title>
+<head>
+    <div class="user">
+    <>{{ users->id }}</name><script></script>
+    </div>
+</head>
+<body>
+    
+<img>
+<script></script>
+<script></script>
+
+</body>
+@endsection
